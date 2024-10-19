dbebe14b44fc0f7f8c4eed0271e824d1e7024477
Shrenik  Borad <shrenikborad@gmail.com>
2024-10-19 01:32:07 -0400

    Add dotenv configuration to load environment variables

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,12 @@
 import app from "./app.mjs";
+// dotenv
+
+import path from "path";
+import { config } from "dotenv";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename);
+config({ path: path.join(__dirname, ".env") });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
