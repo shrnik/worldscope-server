3a9726c4b4e832f8e00c754bb6491a982690823f
Shrenik  Borad <shrenikborad@gmail.com>
2025-03-24 12:29:53 -0400

    🐛 Fix folder error

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -9,11 +9,19 @@ const getFilePath = (cameraId: number | string, url: string) => {
   const tempExt = url.split(".").pop();
   const ext = isValidExt(tempExt) ? tempExt : "jpg";
 
-  // Create a folder structure based on the cameraId and timestamp
-  // path looks like: images/YYYY-MM-DD/hour/cameraId/timestamp.jpg
+  // Create a folder structure based on the cameraId
   const internalPath = path.join(cameraId.toString(), `default.${ext}`);
+  const folderPath = path.resolve(
+    __dirname,
+    "../..",
+    "images",
+    cameraId.toString()
+  );
+
+  fs.mkdirSync(folderPath, { recursive: true });
+
   const filePath = path.resolve(__dirname, "../..", "images", internalPath);
-  fs.mkdirSync(filePath, { recursive: true });
+  // check if the folder exists
 
   // Save the image to the folder
   return {
