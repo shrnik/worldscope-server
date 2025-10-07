4a0cb5117bd22bd22a1e9e1d4c6526497ec96715
Shrenik <shrenikborad@gmail.com>
2025-10-07 18:01:19 -0400

    Refactor contrail classifier to integrate BinaryClassifier; update model loading and prediction methods for neural network compatibility

--- python/contrail_classifier.py
+++ python/contrail_classifier.py
@@ -1,7 +1,10 @@
 # Load pickle file from disk
 import pickle
-import sklearn
 import torch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
 class ContrailClassifier:
     def __init__(self, model_path='python/models/contrails_lr_clip_vitb_patch16.pkl'):
         with open(model_path, 'rb') as f:
@@ -24,11 +27,28 @@ class ContrailClassifier:
         return prob
     
 
+class BinaryClassifier(nn.Module):
+    def __init__(self, input_dim=512, hidden_dim=256):
+        super().__init__()
+        self.classifier = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(hidden_dim, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, 1),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, x):
+        return self.classifier(x)
 
 class ContrailClassifierNN:
     def __init__(self, model_path='python/models/simple_nn_contrails.pth'):
         # Load the neural network model from the specified path
-        self.model = torch.load(model_path)
+        self.model = BinaryClassifier(768, 256)  # Assuming input dimension is 768
+        self.model.load_state_dict(torch.load(model_path))
         self.model.eval()  # Set the model to evaluation mode
 
     def predict(self, embedding):
@@ -48,3 +68,5 @@ class ContrailClassifierNN:
             embedding_tensor = torch.tensor(embedding).float()
             prob = self.model(embedding_tensor).item()
         return prob
+
+
